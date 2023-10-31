44f3a4043830d603061098dfd7fd824c891b902d
Michał Kozak <live.m851@gmail.com>
2023-10-31 22:11:12 +0100

    Styling left side of website

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-PIERWSZA STRONA
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Książki</title>
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="/style/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="left-side">
+        <button class="connect top-button">Połącz</button>
+        <div class="select-main">
+            <h2>Wybierz autora</h2>
+            <div class="select-container">
+                <select id="find-writer">
+                </select>
+            </div>
+            <button class="find-author">Szukaj</button>
+        </div>
+    </div>
+    <div class="right-side">
+
+    </div>
+</body>
+</html>
