79709e0888ea03c190563db40865dae4ad938787
Michał Kozak <live.m851@gmail.com>
2023-10-31 23:44:56 +0100

    set config file to second user

---
+++ config_user2.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname = 'localhost';
+$login = 'user2';
+$password = 'user2';
+$database = 'ksiegarnia1';
+
+// To jest jedyny plik w którym będziemy 
+
+$conn = mysqli_connect($hostname,$login,$password,$database);
+
+?>
