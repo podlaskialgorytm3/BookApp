96dfce1de0e1c1d85ef4bcef7747fe94910ffc7e
Michał Kozak <live.m851@gmail.com>
2023-11-01 00:09:54 +0100

    Add button to add records to db

--- index2.php
+++ index2.php
@@ -35,6 +35,9 @@
         <div class="books-count">
             
         </div>
+        <div class="add-book">
+            +
+        </div>
     </div>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/jsUser2/GetBooks.js"></script>
