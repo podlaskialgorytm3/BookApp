ad59a7e1daed155812a3b1618a06f1cf90e39bea
Michał Kozak <live.m851@gmail.com>
2023-10-31 23:27:10 +0100

    Creating a finding tool

--- getData/get-books-filter.php
+++ getData/get-books-filter.php
@@ -8,12 +8,17 @@ try {
     echo 'Połączenie nie mogło zostać utworzone: ' . $e->getMessage();
     exit();
 }
+$name = $_POST['name'];
+$surname = $_POST['surname'];
 
-$stmt = $pdo->query('SELECT * FROM ksiazki');
+
+$stmt = $pdo->query('SELECT * FROM ksiazki WHERE imieautora = "'.$name.'" and nazwiskoautora = "'.$surname.'" ');
 
 $html = '';
 foreach ($stmt as $row){
-    $html .=  '<option data-surname='.$row["nazwiskoautora"].' data-name='.$row["imieautora"].'>'.$row["imieautora"]." ".$row["nazwiskoautora"].'</option>';
+    $html .=  '<tr>';
+    $html .=  '<td>' . $row['tytul'] . '</td>';
+    $html .=  '</tr>';
 }
 
 sleep(1);
--- js/GetBooks.js
+++ js/GetBooks.js
@@ -9,3 +9,25 @@ function getBooks(){
 $(document).ready(function(){
     getBooks();
 });
+
+const select = document.querySelector("#find-writer")
+const search = document.querySelector(".find-author")
+
+search.addEventListener("click",() => {
+    const selectedOption = select.options[select.selectedIndex];
+    const dataName = selectedOption.getAttribute('data-name');
+    const dataSurname = selectedOption.getAttribute('data-surname');
+    getBooksFilter(dataName,dataSurname)
+})
+function getBooksFilter(dataName,dataSurname){
+    $.ajax({
+        url: "../getData/get-books-filter.php",
+        method: 'POST',
+        data: {
+            name: dataName,
+            surname: dataSurname	
+        }
+    }).done(function( data ) {
+        $('#books').html(data);
+    });
+}
