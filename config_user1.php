beca20a565657fbccb0088f0c7f2f27b3afe8edf
Michał Kozak <live.m851@gmail.com>
2023-10-31 22:13:17 +0100

    Set configuration for user 1

---
+++ config_user1.php
@@ -0,0 +1,12 @@
+<?php
+
+$hostname = 'localhost';
+$login = 'user1';
+$password = 'user1';
+$database = 'ksiegarnia1';
+
+// To jest jedyny plik w którym będziemy 
+
+$conn = mysqli_connect($hostname,$login,$password,$database);
+
+?>
