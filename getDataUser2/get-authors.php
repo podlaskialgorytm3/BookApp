3bdb03d96fce8b898ffc26ff2047d715aa0efd87
Michał Kozak <live.m851@gmail.com>
2023-11-01 12:37:15 +0100

    Fix appearance two and more same authors in select

--- getDataUser2/get-authors.php
+++ getDataUser2/get-authors.php
@@ -10,10 +10,15 @@ try {
 }
 
 $stmt = $pdo->query('SELECT * FROM ksiazki');
-
+$names = [];
+$surnames = [];
 $html = '';
 foreach ($stmt as $row){
-    $html .=  '<option data-surname='.$row["nazwiskoautora"].' data-name='.$row["imieautora"].'>'.$row["imieautora"]." ".$row["nazwiskoautora"].'</option>';
+    if(!(in_array($row["nazwiskoautora"],$surnames) && in_array($row["imieautora"],$names))){
+        $html .=  '<option data-surname='.$row["nazwiskoautora"].' data-name='.$row["imieautora"].'>'.$row["imieautora"]." ".$row["nazwiskoautora"].'</option>';
+    }
+    $names[] = $row["imieautora"];
+    $surnames[] = $row["nazwiskoautora"];
 }
 
 sleep(1);
